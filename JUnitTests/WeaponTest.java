4db8e3f4b27156d0abf1286b3dcecbc5e9530db3
Arkased <kevintmliu@gmail.com>
2017-05-31 22:59:58 -0700

    WeaponTest first commit

---
+++ JUnitTests/WeaponTest.java
@@ -0,0 +1,30 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import architecture.augmentations.equipment.Weapon;
+
+/**
+ *  TODO Write a one-sentence summary of your class here.
+ *  TODO Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author  Kevin Liu
+ *  @version May 31, 2017
+ *  @author  Period: 5
+ *  @author  Assignment: APCS Final
+ *
+ *  @author  Sources: TODO
+ */
+public class WeaponTest
+{
+
+    @Test
+    public void constructorDefault()
+    {
+        Weapon wep = new Weapon();
+        assertTrue(false);
+    }
+}
